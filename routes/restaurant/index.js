c5f70003a8a062ab68c8d3ad8070a7e0f638fd1b
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-05-23 17:24:01 +0200

    routes structure

--- routes/restaurant/index.js
+++ routes/restaurant/index.js
@@ -2,10 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
-const filtersMiddleware = require('../middlewares/filters')
 const getById = require('./handlers/byId')
 
-router.use( filtersMiddleware )
-router.get('/restaurant/:id', getById)
+router.get('/:id', getById)
 
 module.exports = router
--- routes/restaurants/index.js
+++ routes/restaurants/index.js
@@ -2,18 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
-const filtersMiddleware = require('../middlewares/filters')
-
 const getAllRestaurants = require('./handlers/getAll')
 const getByBorough = require('./handlers/byBorough')
 const getByCuisine = require('./handlers/byCuisine')
 const getByNotCuisine = require('./handlers/byNotCuisine')
 
-router.use( filtersMiddleware )
-
-router.get('/restaurants', getAllRestaurants)
-router.get('/restaurants/borough/:borough', getByBorough)
-router.get('/restaurants/cuisine/:cuisine', getByCuisine)
-router.get('/restaurants/cuisine/not/:cuisine', getByNotCuisine)
+router.get('/', getAllRestaurants)
+router.get('/borough/:borough', getByBorough)
+router.get('/cuisine/:cuisine', getByCuisine)
+router.get('/cuisine/not/:cuisine', getByNotCuisine)
 
 module.exports = router
