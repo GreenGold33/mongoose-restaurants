a27921dfaff8b8ea28a953b7913f98b64c845763
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-05-23 16:44:21 +0200

    show & hide filters

--- app.js
+++ app.js
@@ -12,10 +12,24 @@ const Restaurant = require('./models/Restaurant')
 
 mongoose.connect(urlDB)
 
+
 app.get('/restaurants', (req,res) => {
 
+  const { show, hide } = req.query
+  const projection = {}
+
+  if (show) {
+    const fieldsToShow = show.split(',')
+    fieldsToShow.forEach( field => projection[field] = 1 )
+  }
+
+  if (hide) {
+    const fieldsToShow = hide.split(',')
+    fieldsToShow.forEach( field => projection[field] = 0 )
+  }
+
   Restaurant
-    .find()
+    .find( {}, projection )
     .limit(20)
     .then( restaurants => {
       res.json(restaurants)
